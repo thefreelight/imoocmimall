59988d9c22a7583435eb2dc477c431a0cb687a1f
thefreelight <624263170@qq.com>
2020-04-10 11:00:35 +0800

    首页更新

--- mall/src/env.js
+++ mall/src/env.js
@@ -1,19 +1,19 @@
-let baseURL;
-switch(process.env.NODE_ENV) {
-    case 'development':
-        baseURL = 'http://dev-mall-pre.springboot.cn/api';
-        break;
-    case 'test':
-        baseURL = 'http://test-mall-pre.springboot.cn/api';
-        break;    
-    case 'prodution':
-        baseURL = 'http://mall-pre.springboot.cn/api';
-        break;
-    default:
-        baseURL = 'http://mall-pre.springboot.cn/api';
-        break;
-}
+// let baseURL;
+// switch(process.env.NODE_ENV) {
+//     case 'development':
+//         baseURL = 'http://mall-pre.springboot.cn/api';
+//         break;
+//     case 'test':
+//         baseURL = 'http://test-mall-pre.springboot.cn/api';
+//         break;    
+//     case 'prodution':
+//         baseURL = 'http://mall-pre.springboot.cn/api';
+//         break;
+//     default:
+//         baseURL = 'http://mall-pre.springboot.cn/api';
+//         break;
+// }
 
-export default{
-    baseURL
-}
+// export default{
+//     baseURL
+// }
